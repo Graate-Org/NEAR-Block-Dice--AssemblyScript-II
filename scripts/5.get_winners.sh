852d082dc161c4c1c905ee58a631c68426fdc2d9
IkehAkinyemi <mrikehchukwuka@gmail.com>
2021-12-02 05:37:15 +0100

    Working on the getWinners method

--- scripts/5.get_winners.sh
+++ scripts/5.get_winners.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-near call $CONTRACT getWinners --account_id $OWNER '{"gameId": ""}'
+near call $CONTRACT getWinners --account_id $OWNER '{"gameId": "BD-1048959492"}'
 
 exit 0
